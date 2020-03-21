24f1d61598aaa2b06e87e1faf206236a545b0f99
valt123 <valttei@hotmail.fi>
2020-03-21 15:11:56 +0200

    Initial stuff

---
+++ GameClient/Multiplayer shooter/Assets/scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public static Dictionary<int, PlayerManager> players = new Dictionary<int, PlayerManager>();
+
+    public GameObject localPlayerPrefab;
+    public GameObject playerPrefab;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        if(instance != this)
+        {
+            Debug.Log("Instance already exists");
+            Destroy(gameObject);
+        }
+    }
+
+    public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
+    {
+        GameObject _player;
+        if (_id == Client.instance.myId)
+        {
+            _player = Instantiate(localPlayerPrefab, _position, _rotation);
+        }
+        else
+        {
+            _player = Instantiate(playerPrefab, _position, _rotation);
+        }
+
+        _player.GetComponent<PlayerManager>().Initialize(_id, _username);
+        players.Add(_id, _player.GetComponent<PlayerManager>());
+    }
+}
---
+++ GameClient/Multiplayer shooter/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Jump stuff
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    public float jumpVelocity;
+
+    //Movement stuff
+    public float movementSpeed;
+    Vector3 movement;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        movement.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        movement.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        rb.position += movement;
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+        {
+            rb.velocity += Vector3.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
